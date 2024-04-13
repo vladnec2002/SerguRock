663df10bc526a83bfa97bb20ebe41b1b85e455e0
JeffRie23 <104787390+JeffRie23@users.noreply.github.com>
2024-04-13 17:57:15 +0300

    Mecanici de baza
    Piatra s-a rostogolit cu succes doamnelor si domnilor

---
+++ RockGame/RockGame/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]float playerInputHorizontal;
+    [SerializeField] float speed = 5;
+    [SerializeField] float jumpForce = 20;
+    Rigidbody2D playerRb;
+    [SerializeField] bool isOnGround;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        playerInputHorizontal = Input.GetAxis("Horizontal");
+        if(Input.GetKeyDown(KeyCode.Space) && isOnGround)
+        {
+            playerRb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            isOnGround = false;
+        }
+        
+        playerRb.AddForce(Vector2.right * playerInputHorizontal);
+        
+        
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.CompareTag("Ground"))
+        {
+            isOnGround = true;
+        }
+    }
+}
